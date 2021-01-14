5c7858549ae36bedceeae99c5df25b22d4237f27
Yuriy Nikolaev <warlokk83@gmail.com>
2021-01-14 19:55:23 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        long balance = 2_000_000_000;
+        double transaction = 500_000_000.57;
+        double newBalance = balance + transaction;
+        System.out.println(newBalance);
+    }
+}
